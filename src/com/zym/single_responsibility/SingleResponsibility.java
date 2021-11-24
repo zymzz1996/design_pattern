f763209e08794e31a4d83bae9afabc24ae3fb57c
周雨蒙 <71252200+zymzz1996@users.noreply.github.com>
2021-11-24 09:20:05 +0800

    Initial commit

---
+++ src/com/zym/single_responsibility/SingleResponsibility.java
@@ -0,0 +1,41 @@
+package com.zym.single_responsibility;
+
+import com.sun.xml.internal.ws.api.pipe.ServerTubeAssemblerContext;
+
+/**
+ * @author zhouyumeng
+ * @date 2021/11/24
+ **/
+public class SingleResponsibility {
+
+    public static void main(String[] args) {
+        RoadVehicle roadVehicle = new RoadVehicle();
+        roadVehicle.run("汽车");
+        WaterVehicle waterVehicle = new WaterVehicle();
+        waterVehicle.run("轮船");
+        AirVehicle airVehicle = new AirVehicle();
+        airVehicle.run("飞机");
+    }
+
+    static class RoadVehicle{
+
+        public void run(String name) {
+            System.out.println(name + "在公路上跑!");
+        }
+    }
+
+    static class WaterVehicle{
+
+        public void run(String name) {
+            System.out.println(name + "在水中跑!");
+        }
+    }
+
+    static class AirVehicle{
+
+        public void run(String name) {
+            System.out.println(name + "在空中跑!");
+        }
+    }
+
+}
