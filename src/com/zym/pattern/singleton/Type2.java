ac15ef716343fa61dd5b035b77ac3a78a106d0ad
周雨蒙 <1034002754@qq.com>
2021-12-05 10:56:34 +0800

    单例模式：饿汉式(静态代码块)

---
+++ src/com/zym/pattern/singleton/Type2.java
@@ -0,0 +1,37 @@
+package com.zym.pattern.singleton;
+
+/**
+ * 饿汉式(静态代码块)
+ * @author: zhouyumeng
+ * @date: 2021-12-04
+ **/
+public class Type2 {
+
+    public static void main(String[] args) {
+        Singleton instance = Singleton.getInstance();
+        Singleton instance1 = Singleton.getInstance();
+        System.out.println(instance == instance1);
+        System.out.println(instance.hashCode());
+        System.out.println(instance1.hashCode());
+    }
+
+
+    static class Singleton{
+
+        private Singleton() {
+
+        }
+
+        private final static Singleton INSTANCE;
+
+        static {
+            INSTANCE = new Singleton();
+        }
+
+        public static Singleton getInstance() {
+            return INSTANCE;
+        }
+
+    }
+
+}
